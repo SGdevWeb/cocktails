846afcbbce47323a01e4b23bbe29e2c14d68bc05
SGdevWeb <samuel.gustin.dev@gmail.com>
2023-06-18 22:10:53 +0200

    Creation of a cocktail-container component to pass data between cocktail-list and cocktail-details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,21 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { CocktailListComponent } from './cocktail-list/cocktail-list.component';
-import { CocktailDetailsComponent } from './cocktail-details/cocktail-details.component';
+import { CocktailListComponent } from './cocktail-container/cocktail-list/cocktail-list.component';
+import { CocktailContainerComponent } from './cocktail-container/cocktail-container.component';
+import { CocktailDetailsComponent } from './cocktail-container/cocktail-details/cocktail-details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     CocktailListComponent,
-    CocktailDetailsComponent
+    CocktailDetailsComponent,
+    CocktailContainerComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/cocktail-container/cocktail-container.component.ts
+++ src/app/cocktail-container/cocktail-container.component.ts
@@ -2,33 +2,39 @@ import { Component, OnInit } from '@angular/core';
 import { Cocktail } from '../entities/cocktail';
 
 @Component({
-  selector: 'app-cocktail-list',
-  templateUrl: './cocktail-list.component.html',
-  styleUrls: ['./cocktail-list.component.scss'],
+  selector: 'app-cocktail-container',
+  templateUrl: './cocktail-container.component.html',
+  styleUrls: ['./cocktail-container.component.scss'],
 })
-export class CocktailListComponent implements OnInit {
-  cocktails: Cocktail[] = [
+export class CocktailContainerComponent implements OnInit {
+  public cocktails: Cocktail[] = [
     {
       name: 'Mojito',
-      img: 'https://www.hangoverweekends.co.uk/media/15505/mojito.jpg?width=500&height=375',
+      img: 'https://www.hangoverweekends.co.uk/uploads/images/mojito.jpg',
       description:
         "Le Mojito complète parfaitement l'été avec son goût frais de menthe. Le mélange de rhum blanc, de menthe, de jus de citron vert, de sucre et d'eau gazeuse est vif, avec une teneur en alcool relativement faible ; l'eau gazeuse peut être remplacée par du Sprite ou du 7-up. Lorsque vous préparez un mojito, écrasez toujours les feuilles de menthe au lieu de les couper en dés afin de libérer les huiles qui contribueront à renforcer la saveur mentholée. ",
     },
     {
       name: 'Cosmopolitan',
-      img: 'https://www.hangoverweekends.co.uk/media/15507/gallery-1430408520-dmg-cosmopolitan-cocktail-001.jpg?width=330px&height=407px',
+      img: 'https://www.hangoverweekends.co.uk/uploads/images/cosmo.jpg',
       description:
         "Ce mélange acidulé de vodka, de triple sec, de jus de citron vert et de jus de canneberge a réussi à supplanter le vénérable screwdriver, car de nombreux buveurs de vodka préfèrent le goût plus net et légèrement acidulé du Cosmopolitan. Les clés de la préparation d'un Cosmopolitan sont une bonne marque de jus de canneberge et le Cointreau Triple Sec, deux éléments essentiels de la boisson.",
     },
     {
       name: 'Mai Tai',
-      img: 'https://www.hangoverweekends.co.uk/media/15506/mm-cocktail-guide-maitai-590x375.jpg?width=434px&height=276px',
+      img: 'https://www.cocktail.fr/wp-content/uploads/2017/05/mai-tai.jpg',
       description:
         "Le Mai Tai est un cocktail de style polynésien qui a un goût tropical fruité, sucré et vibrant. Le mélange de rhum clair et foncé, de curaçao orange, de sirop d'orgeat et de jus de citron vert est un symbole de la culture tahitienne depuis la création de cette boisson.",
     },
   ];
 
-  constructor() {}
+  public selectedCocktail!: Cocktail;
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.selectedCocktail = this.cocktails[0];
+  }
+
+  public selectCocktail(index: number): void {
+    this.selectedCocktail = this.cocktails[index];
+  }
 }
